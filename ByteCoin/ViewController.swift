8e330385885f545b2513487dd69ecdb4e2ba7507
Vladimir <motiw@icloud.com>
2023-02-22 23:11:03 +0600

    UI

--- ByteCoin/ViewController.swift
+++ ByteCoin/ViewController.swift
@@ -1,19 +1,93 @@
-//
-//  ViewController.swift
-//  ByteCoin
-//
-//  Created by Vladimir Fibe on 20.02.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
-
+    let cities = ["Almaty", "Astana", "Tomsk"]
+    let titleLabel = UILabel().then {
+        $0.text = "ByteCoin"
+        $0.font = .systemFont(ofSize: 50, weight: .thin)
+        $0.textAlignment = .center
+        $0.textColor = UIColor(named: "Title Color")
+    }
+    let bitcoinLabel = UILabel().then {
+        $0.text = "..."
+        $0.textColor = .white
+        $0.textAlignment = .right
+        $0.font = .systemFont(ofSize: 25)
+    }
+    let imageView = UIImageView(image: UIImage(systemName: "bitcoinsign.circle.fill")).then {
+        $0.tintColor = UIColor(named: "Icon Color")
+    }
+    let currencyLabel = UILabel().then {
+        $0.text = "USD"
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 25)
+    }
+    let currencyPicker = UIPickerView()
+    lazy var titleStack = UIStackView(arrangedSubviews: [imageView, bitcoinLabel, currencyLabel]).then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.spacing = 10
+    }
+    lazy var titleView = UIView().then {
+        $0.addSubview(titleStack)
+        $0.backgroundColor = .tertiaryLabel
+        $0.layer.cornerRadius = 40
+    }
+    lazy var stack = UIStackView(arrangedSubviews: [titleLabel, titleView, UIView(), currencyPicker]).then {
+        $0.spacing = 25
+        $0.axis = .vertical
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        layoutViews()
+        configureAppearance()
+    }
 }
 
+extension ViewController {
+    func setupViews() {
+        view.addSubview(stack)
+    }
+    
+    func layoutViews() {
+        let margin = view.layoutMarginsGuide
+
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: margin.topAnchor),
+            stack.leadingAnchor.constraint(equalTo: margin.leadingAnchor),
+            stack.trailingAnchor.constraint(equalTo: margin.trailingAnchor),
+            stack.bottomAnchor.constraint(equalTo: margin.bottomAnchor),
+            
+            titleStack.topAnchor.constraint(equalTo: titleView.topAnchor),
+            titleStack.leadingAnchor.constraint(equalTo: titleView.leadingAnchor),
+            titleStack.trailingAnchor.constraint(equalTo: titleView.trailingAnchor, constant: -10),
+            titleStack.bottomAnchor.constraint(equalTo: titleView.bottomAnchor),
+            currencyPicker.heightAnchor.constraint(equalToConstant: 216),
+            imageView.heightAnchor.constraint(equalToConstant: 80),
+            imageView.widthAnchor.constraint(equalToConstant: 80)
+            
+            
+        ])
+    }
+    
+    func configureAppearance() {
+        view.backgroundColor = UIColor(named: "Background Color")
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        cities.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        cities[row]
+    }
+}
